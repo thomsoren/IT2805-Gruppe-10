01fc3ac09ea9eb49757ae6bd706c8d3cbdba6c73
Jo Tremoen <jo.nass.tremoen@gmail.com>
2023-10-30 13:50:33 +0100

    ok

--- P3/universal.js
+++ P3/universal.js
@@ -1,43 +1,42 @@
-/*let footer = document.getElementById("footer")
-document.body.appendChild(footer.content);
-<<<<<<< HEAD
-
-document.addEventListener("DOMContentLoaded", function() {
-    loadComponent("header-placeholder", "header.html");
-    loadComponent("footer-placeholder", "footer.html");
-});
-
-function loadComponent(placeholderId, url) {
-    fetch(url).then(response => {
-        return response.text();
-    }).then(data => {
-        document.getElementById(placeholderId).innerHTML = data;
-    }).catch(error => {
-        console.error('Error loading component:', error);
-    });
-}
-=======
-//funksjon for å gjøre html om til en string.
-
-function createTemplate(html){
-    const template = document.createElement("template");
-    template.innerHTML = html.trim();
-    return template.content.firstChild;
-}
-<<<<<<< HEAD
-const Nav = createTemplate('<div id="menu">\n' +
-    '        <img id="logo" src="img/logo.png" alt="logo"/>\n' +
-    '        <button id="cart_button"><img id="cart_img"src="img/cart.png" alt="cart" ></button>\n' +
-    '        <div id="menu_div">\n' +
-    '          <button class="menu_link">Home</button>\n' +
-    '          <button class="menu_link">Buy</button>\n' +
-    '          <button class="menu_link">Sell</button>\n' +
-    '          <button class="menu_link">About us</button>\n' +
-    '          <button class="menu_link">Contact</button>\n' +
-    '        </div>'
-)
- */
-const html = `
-<!--legg html her--!>
+const meny = `
+<div id="menu">
+        <img id="logo" src="img/logo.png" alt="logo"/>
+        <button id="cart_button"><img id="cart_img"src="img/cart.png" alt="cart" ></button>
+        <div id="menu_div">
+          <button class="menu_link">Home</button>
+          <button class="menu_link">Buy</button>
+          <button class="menu_link">Sell</button>
+          <button class="menu_link">About us</button>
+          <button class="menu_link">Contact</button>
+</div>
 `
-document.querySelector('#div').innerHTML = html
+document.querySelector('#hode').innerHTML = meny
+
+const footer = `
+<div class="flex-container">
+      <div class="flex-box">
+        <h1>Address & postal office</h1>
+        <p>Klokkeveien 123 A</p>
+        <p>7052, Trondheim</p>
+        <p>Norway</p>
+      </div>
+      <div class="flex-box">
+        <h1>Contact</h1>
+        <p><img src="img/telephonew.png" alt="phone" id="phone"> +47 901 23 456</p>
+        <p id="emailtekst"><img src="./img/Message.png" alt="message" id="message">Somethingsomething@gmail.com</p>
+      </div>
+      <div class="flex-box">
+        <h1>Sitemap</h1>
+          <button class="footer_link">Home</button>
+          <br/>
+          <button class="footer_link">Buy</button>
+          <br/>
+          <button class="footer_link">Sell</button>
+          <br/>
+          <button class="footer_link">About us</button>
+          <br/>
+          <button class="footer_link">Contact</button>
+      </div>
+    </div>
+`
+document.querySelector('#feet').innerHTML = footer
