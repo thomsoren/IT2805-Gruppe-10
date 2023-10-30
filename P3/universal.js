6959e0dd58f5f47f15fbbc1660fbb7817c88fb4a
Ludvig <iluddeludde@gmail.com>
2023-10-30 13:25:45 +0100

    .

--- P3/universal.js
+++ P3/universal.js
@@ -1,4 +1,4 @@
-let footer = document.getElementById("footer")
+/*let footer = document.getElementById("footer")
 document.body.appendChild(footer.content);
 <<<<<<< HEAD
 
@@ -18,23 +18,26 @@ function loadComponent(placeholderId, url) {
 }
 =======
 //funksjon for å gjøre html om til en string.
-/*
+
 function createTemplate(html){
     const template = document.createElement("template");
     template.innerHTML = html.trim();
-    return template.content.firstElementChild;
+    return template.content.firstChild;
 }
-const Nav = createTemplate(`
-        <div id="menu">
-        <img id="logo" src="img/logo.png" alt="logo"/>
-        <button id="cart_button"><img id="cart_img"src="img/cart.png" alt="cart" ></button>
-        <div id="menu_div">
-          <button class="menu_link">Home</button>
-          <button class="menu_link">Buy</button>
-          <button class="menu_link">Sell</button>
-          <button class="menu_link">About us</button>
-          <button class="menu_link">Contact</button>
-        </div>
-´)
-*/
->>>>>>> origin
+<<<<<<< HEAD
+const Nav = createTemplate('<div id="menu">\n' +
+    '        <img id="logo" src="img/logo.png" alt="logo"/>\n' +
+    '        <button id="cart_button"><img id="cart_img"src="img/cart.png" alt="cart" ></button>\n' +
+    '        <div id="menu_div">\n' +
+    '          <button class="menu_link">Home</button>\n' +
+    '          <button class="menu_link">Buy</button>\n' +
+    '          <button class="menu_link">Sell</button>\n' +
+    '          <button class="menu_link">About us</button>\n' +
+    '          <button class="menu_link">Contact</button>\n' +
+    '        </div>'
+)
+ */
+const html = `
+<!--legg html her--!>
+`
+document.querySelector('#div').innerHTML = html
