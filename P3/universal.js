caabf750587b72f5d4852b9b01c0e22c04837628
jonasgjo <jonasgjo@stud.ntnu.no>
2023-10-30 12:33:51 +0100

    Temporary

--- P3/universal.js
+++ P3/universal.js
@@ -1,3 +1,14 @@
-console.log("moren din")
 
+const getNav = `
+<div id="menu">
+        <img id="logo" src="img/logo.png" alt="logo"/>
+        <button id="cart_button"><img id="cart_img"src="img/cart.png" alt="cart" ></button>
+        <div id="menu_div">
+          <button class="menu_link">Home</button>
+          <button class="menu_link">Buy</button>
+          <button class="menu_link">Sell</button>
+          <button class="menu_link">About us</button>
+          <button class="menu_link">Contact</button>
+        </div>
+`
 
