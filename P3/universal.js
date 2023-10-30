6a8c7d72ed92bb95149a605283a6f2b9bdcda70f
Ludvig <iluddeludde@gmail.com>
2023-10-30 12:18:31 +0100

    fixa header

--- P3/universal.js
+++ P3/universal.js
@@ -2,3 +2,17 @@ let nav= document.getElementById("nav")
 let footer = document.getElementById("footer")
 document.body.appendChild(footer.content);
 
+document.addEventListener("DOMContentLoaded", function() {
+    loadComponent("header-placeholder", "header.html");
+    loadComponent("footer-placeholder", "footer.html");
+});
+
+function loadComponent(placeholderId, url) {
+    fetch(url).then(response => {
+        return response.text();
+    }).then(data => {
+        document.getElementById(placeholderId).innerHTML = data;
+    }).catch(error => {
+        console.error('Error loading component:', error);
+    });
+}
