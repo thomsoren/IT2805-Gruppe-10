e995802c92e8fef26ef6348c34252c29c2308acb
Jo Tremoen <jo.nass.tremoen@gmail.com>
2023-10-30 12:43:45 +0100

    committing small changes

--- P3/universal.js
+++ P3/universal.js
@@ -1,3 +1,51 @@
 footer = getElementbyid("footer") 
 document.body.appendChild(footer.content);
-
+const html = `
+    <div class = "wrapper">
+    <nav>
+      <div id="menu">
+        <img id="logo" src="img/logo.png" alt="logo"/>
+        <button id="cart_button"><img id="cart_img"src="img/cart.png" alt="cart" ></button>
+        <div id="menu_div">
+          <button class="menu_link">Home</button>
+          <button class="menu_link">Buy</button>
+          <button class="menu_link">Sell</button>
+          <button class="menu_link">About us</button>
+          <button class="menu_link">Contact</button>
+        </div>
+    </nav>
+    <div class="Content">
+      <h1>Test</h1>
+    </div>
+    </div>
+    <script src="universal.js"></script>
+  </body>
+  <footer>
+    <div class="flex-container">
+      <div class="flex-box">
+        <h1>Address & postal office</h1>
+        <p>Klokkeveien 123 A</p>
+        <p>7052, Trondheim</p>
+        <p>Norway</p>
+      </div>
+      <div class="flex-box">
+        <h1>Contact</h1>
+        <p><img src="img/telephonew.png" alt="phone" id="phone"> +47 901 23 456</p>
+        <p id="emailtekst"><img src="./img/Message.png" alt="message" id="message">Somethingsomething@gmail.com</p>
+      </div>
+      <div class="flex-box">
+        <h1>Sitemap</h1>
+          <button class="footer_link">Home</button>
+          <br/>
+          <button class="footer_link">Buy</button>
+          <br/>
+          <button class="footer_link">Sell</button>
+          <br/>
+          <button class="footer_link">About us</button>
+          <br/>
+          <button class="footer_link">Contact</button>
+      </div>
+    </div>
+  </footer>
+`
+document.querySelector('#div').innerHTML = html
